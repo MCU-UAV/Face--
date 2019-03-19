7c51e7cea666362ef82b3332f111b3649e769d84
Daybreak <771203553@QQ.COM>
2019-03-19 17:37:20 +0800

    第一次提交

---
+++ getPic.py
@@ -0,0 +1,80 @@
+import base64
+import json
+import requests
+
+
+class BaiduPicIndentify:
+    def __init__(self, img):
+        self.AK = "aotCVGwzQrGUhjbBLUgHAfnw"
+        self.SK = "waDjfTeo0yNNIkP1vlW8q0RMkWgSyaO6"
+        self.img_src = img
+        self.headers = {
+            "Content-Type": "application/json; charset=UTF-8"
+        }
+        self.face_num = 0
+        self.age=-1                     #double
+        self.beauty=-1                  #int64 0-100
+        self.race='none'                #yellow: 黄种人 white: 白种人 black:黑种人 arabs: 阿拉伯人
+        self.gender='none'              #male:男性 female:女性
+        self.expression='none'          #none:不笑；smile:微笑；laugh:大笑
+        self.face_shape=0
+        self.emotion='none'
+        self.glasses='none'
+
+
+    def faceout(self):
+        print('人数: {} 年龄: {} 颜值: {} 脸型: {} \n人种: {} 性别: {} 表情: {} 情绪: {} 眼镜:{}'.format(self.face_num,self.age,self.beauty,self.face_shape,self.race,self.gender,self.expression,self.emotion,self.glasses));
+
+    def get_accessToken(self):
+        host = 'https://aip.baidubce.com/oauth/2.0/token?grant_type=client_credentials&client_id=' + self.AK + '&client_secret=' + self.SK
+        response = requests.get(host, headers=self.headers)
+        json_result = json.loads(response.text)
+        return json_result['access_token']
+
+    def img_to_BASE64(self):
+            base64_data = base64.b64encode(self.img_src)
+            return base64_data
+
+    def detect_face(self):
+        # 人脸检测与属性分析
+        img_BASE64 = self.img_to_BASE64()
+        request_url = "https://aip.baidubce.com/rest/2.0/face/v3/detect"
+        post_data = {
+            "image": img_BASE64,
+            "image_type": "BASE64",
+            "face_field": "gender,age,beauty,race,expression,face_shape,glasses,emotion",
+            "face_type": "LIVE"
+        }
+        access_token = self.get_accessToken()
+        request_url = request_url + "?access_token=" + access_token
+        response = requests.post(url=request_url, data=post_data, headers=self.headers)
+        json_result = json.loads(response.text)
+        if json_result['error_msg'] != 'SUCCESS':
+            print(json_result['error_msg'])
+        if json_result['error_msg'] != 'pic not has face' and json_result['error_msg'] == 'SUCCESS':
+           #print("图片中包含人脸数：", json_result['result']['face_num'])
+           #print("图片中包含人物年龄：", json_result['result']['face_list'][0]['age'])
+           #print("图片中包含人物颜值评分：", json_result['result']['face_list'][0]['beauty'])
+           #print("图片中包含人物性别：", json_result['result']['face_list'][0]['gender']['type'])
+           #print("图片中包含人物种族：", json_result['result']['face_list'][0]['race']['type'])
+           ##print("图片中包含人物表情：", json_result['result']['face_list'][0]['expression']['type'])
+           self.face_num=json_result['result']['face_num']
+           self.age=json_result['result']['face_list'][0]['age'];
+           self.beauty=json_result['result']['face_list'][0]['beauty'];
+           self.gender=json_result['result']['face_list'][0]['gender']['type'];
+           self.race=json_result['result']['face_list'][0]['race']['type'];
+           self.expression=json_result['result']['face_list'][0]['expression']['type']
+           self.face_shape=json_result['result']['face_list'][0]['face_shape']['type']
+           self.emotion=json_result['result']['face_list'][0]['emotion']['type']
+           self.glasses=json_result['result']['face_list'][0]['glasses']['type']
+
+
+
+
+def getPic(url):
+    #img_src = input('请输入需要检测的本地图片路径:')
+    #img_src="https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=1240817690,4214518707&fm=26&gp=0.jpg"     #test url
+
+    baiduDetect = BaiduPicIndentify(url)
+    baiduDetect.detect_face()
+    return baiduDetect;
---
+++ main.py
@@ -0,0 +1,78 @@
+import urllib.request
+import re
+from getPic import BaiduPicIndentify
+from bs4 import BeautifulSoup
+import random
+import requests
+import lxml
+import time
+import os
+from scrapy.selector import Selector
+url='http://www.meizitu.com' #需要设置首页地址
+page_num = 10 #所要爬取的页数
+
+page_url=[]
+for i in range(1,page_num+1):
+    page_url.append(url  + '/a/list_1_%d.html'% (i))
+
+my_headers = [
+    "Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.153 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:30.0) Gecko/20100101 Firefox/30.0",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_2) AppleWebKit/537.75.14 (KHTML, like Gecko) Version/7.0.3 Safari/537.75.14",
+    "Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.2; Win64; x64; Trident/6.0)",
+    'Mozilla/5.0 (Windows; U; Windows NT 5.1; it; rv:1.8.1.11) Gecko/20071127 Firefox/2.0.0.11',
+    'Opera/9.25 (Windows NT 5.1; U; en)',
+    'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; .NET CLR 1.1.4322; .NET CLR 2.0.50727)',
+    'Mozilla/5.0 (compatible; Konqueror/3.5; Linux) KHTML/3.5.5 (like Gecko) (Kubuntu)',
+    'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.8.0.12) Gecko/20070731 Ubuntu/dapper-security Firefox/1.5.0.12',
+    'Lynx/2.8.5rel.1 libwww-FM/2.14 SSL-MM/1.4.1 GNUTLS/1.2.9',
+    "Mozilla/5.0 (X11; Linux i686) AppleWebKit/535.7 (KHTML, like Gecko) Ubuntu/11.04 Chromium/16.0.912.77 Chrome/16.0.912.77 Safari/535.7",
+    "Mozilla/5.0 (X11; Ubuntu; Linux i686; rv:10.0) Gecko/20100101 Firefox/10.0 "
+]
+def url_open(url):
+    req = urllib.request.Request(url)
+    req.add_header('User-Agent', random.choice(my_headers))
+    response = urllib.request.urlopen(req).read().decode('gbk')
+    return response;
+
+for index_list in page_url:   #每一页
+    response = url_open(index_list)
+    html = lxml.etree.HTML(response)
+    path_name=html.xpath('//*[@id="maincontent"]/div/ul/li/div/div/a/img/@alt')
+    src_url = html.xpath('//*[@id="maincontent"]/div/ul/li/div/div/a/@href')
+   
+    for index_src in src_url:   #每一个缩略图
+        print(index_src)
+        res =  url_open(index_src)
+        imghtml = lxml.etree.HTML(res)
+        src_data = imghtml.xpath('//*[@id="picture"]/p/img/@src')  #详情页的图片地址
+        src_name = imghtml.xpath('//*[@id="picture"]/p/img/@alt')  #详情页的图片名
+         #如果不存在这个文件夹
+        dir_name = str(path_name[src_url.index(index_src)])[3:10]
+        if(not os.path.exists('d:/all/'+dir_name)): 
+            os.makedirs('D:/all/'+dir_name)
+        if src_data == [] and src_name == []: #如果数据为空，跳过
+            pass
+        else:
+            for index_img in src_data:
+                
+                pic_name = src_name[src_data.index(index_img)]
+                img_path = 'd:/all/'+dir_name+'/'+pic_name+'.jpg'
+                if os.path.isfile(img_path):
+                    print('{} 中的 {} 已经存在!'.format(dir_name,pic_name))
+                else:
+                    
+                    try:
+                        img_data = requests.get(index_img)
+                        face = BaiduPicIndentify(img_data.content)
+                        face.detect_face()
+                        face.faceout()
+                        with open(img_path, 'wb')as f:
+                            f.write(img_data.content)
+                            print("正在保存{}中的 {}".format(dir_name, pic_name))
+                            f.close()
+                    except OSError:
+                        continue
+               
+#print(response)
